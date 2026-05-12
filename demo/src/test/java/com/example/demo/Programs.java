2076e79da0289a8ae61777ddca5ae8ff12386d46
anandyadav5ster@gmail.com <anandyadav5ster@gmail.com>
2026-05-12 13:55:09 +0530

    new coded for programs

--- demo/src/test/java/com/example/demo/Programs.java
+++ demo/src/test/java/com/example/demo/Programs.java
@@ -4,22 +4,55 @@ import java.util.*;
 public class Programs {
 
 	public static void main(String[] args) {
-		int[] a = {4, 7, 2, 3, 8, 9, 5, 2};
-		ascendingOrder(a);
+		
+		ascendingOrder();
+		descendingOrder();
+		removeDuplicate();
 
 	}
 
 	// ascending order
-	public static void ascendingOrder(int[] arr) {
+	public static void ascendingOrder() {
+		int[] arr = {4, 7, 2, 3, 8, 9, 5, 2};
 		Set<Integer> a = new TreeSet<>();
 		for(int x : arr) {
 			a.add(x);
 		}
 	
-		System.out.println(a);
+		System.out.println("Sort in ascending order " +a);
 	}
 	
+	// public static void descending order
+	public static void descendingOrder() {
+		int[] arr = {4, 7, 2, 3, 8, 9, 5, 2};
+		Set<Integer> a = new TreeSet<>(Collections.reverseOrder());
+		for(int x : arr) {
+			a.add(x);
+		}
+		System.out.println("Sort in descending order "+a);
+	}
+	
+	
 	public static void removeDuplicate() {
-		
+		String item = "Deelhhiii";
+		String reverse = "";
+		char[] ch = item.toCharArray();
+		for(int i = item.length()-1;i>=0;i--) {
+			reverse =reverse+item.charAt(i);
+		}
+		System.out.println("Reverse of the string is " +reverse);
+		System.out.println("======Remove Duplicates============");
+		HashMap<Character,Integer> hmap = new HashMap<>();
+		for(Character c: ch) {
+			hmap.put(c, (hmap.getOrDefault(c, 0))+1);
+		}
+		System.out.println(hmap);
+		StringBuilder sb = new StringBuilder();
+		for(Map.Entry<Character, Integer> map : hmap.entrySet()) {
+			if(map.getValue() ==1) {
+				sb.append(map.getKey()).append(map.getValue());
+			}
+		}
+		System.out.print("Count the value and output in key Value format " +sb);
 	}
 }
